7568e83457a5fc047bff69dd890b7982828ded92
levig <levi.goossens107@gmail.com>
2026-03-02 21:00:17 +0100

    make encryption service interface

---
+++ Encryptietool.WebApp/WebApplication1/Services/Interfaces/IAesEncryptionService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using WebApplication1.Entities;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services.Interfaces
+{
+    public interface IAesEncryptionService
+    {
+        public EncryptionResult Encrypt(string plaintext, byte[] key, byte[] iv, CipherMode mode);
+
+        public string Decrypt(byte[] ciphertext, byte[] key, byte[] iv, CipherMode mode);
+    }
+}
