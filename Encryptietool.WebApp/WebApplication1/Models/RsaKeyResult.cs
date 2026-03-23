f43e5baca26f82f602128d19d9cd3d435473e828
NielBeynaertsPXL <niel.beynaerts@student.pxl.be>
2026-03-23 23:19:07 +0100

    Part 1: Key generation

---
+++ Encryptietool.WebApp/WebApplication1/Models/RsaKeyResult.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class RsaKeyResult
+    {
+        public string PublicKeyPem { get; set; } = string.Empty;
+        public string PrivateKeyPem { get; set; } = string.Empty;
+        public int KeySize { get; set; }
+    }
+}
---
+++ Encryptietool.WebApp/WebApplication1/Services/Interfaces/IAesKeyGenerator.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services.Interfaces
+{
+    public interface IAesKeyGenerator
+    {
+        AesKeyResult GenerateKey(int keySize);
+    }
+}
