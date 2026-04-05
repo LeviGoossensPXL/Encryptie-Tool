dbef8aa30dad2f67f7985368c929dfee81296774
levig <levi.goossens107@gmail.com>
2026-04-05 10:07:40 +0200

    configureAes private method

--- Encryptietool.WebApp/WebApplication1/Services/AesEncryptionService.cs
+++ Encryptietool.WebApp/WebApplication1/Services/AesEncryptionService.cs
@@ -8,11 +8,8 @@ namespace WebApplication1.Services
     {
         public AesEncryptionResult Encrypt(string plaintext, string key, string iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            using Aes aes = Aes.Create();
-            aes.Key = Convert.FromBase64String(key);
-            aes.IV = Convert.FromBase64String(iv);
-            aes.Mode = cipherMode;
-            aes.Padding = paddingMode;
+            using Aes aes = ConfigureAes(key, iv, cipherMode, paddingMode);
+            
             using ICryptoTransform encryptor = aes.CreateEncryptor();
 
             using MemoryStream memoryStream = new();
@@ -31,11 +28,8 @@ namespace WebApplication1.Services
 
         public AesDecryptionResult Decrypt(string ciphertext, string key, string iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            using Aes aes = Aes.Create();
-            aes.Key = Convert.FromBase64String(key);
-            aes.IV = Convert.FromBase64String(iv);
-            aes.Mode = cipherMode;
-            aes.Padding = paddingMode;
+            using Aes aes = ConfigureAes(key, iv, cipherMode, paddingMode);
+            
             using ICryptoTransform decryptor = aes.CreateDecryptor();
 
             using MemoryStream memoryStream = new(Convert.FromBase64String(ciphertext));
@@ -45,17 +39,16 @@ namespace WebApplication1.Services
             {
                 DecryptedText = streamReader.ReadToEnd()
             };
+            
             return result;
         }
 
         public AesEncryptionResult EncryptFile(FileInfo file, string key, string iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
             var result = new AesEncryptionResult();
-            using Aes aes = Aes.Create();
-            aes.Key = Convert.FromBase64String(key);
-            aes.IV = Convert.FromBase64String(iv);
-            aes.Mode = cipherMode;
-            aes.Padding = paddingMode;
+            
+            using Aes aes = ConfigureAes(key, iv, cipherMode, paddingMode);
+            
             using ICryptoTransform encryptor = aes.CreateEncryptor();
 
             using FileStream inFs = new(file.FullName, FileMode.Open);
@@ -77,11 +70,9 @@ namespace WebApplication1.Services
         public AesDecryptionResult DecryptFile(FileInfo file, string key, string iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
             var result = new AesDecryptionResult();
-            using Aes aes = Aes.Create();
-            aes.Key = Convert.FromBase64String(key);
-            aes.IV = Convert.FromBase64String(iv);
-            aes.Mode = cipherMode;
-            aes.Padding = paddingMode;
+            
+            using Aes aes = ConfigureAes(key, iv, cipherMode, paddingMode);
+            
             using ICryptoTransform decryptor = aes.CreateDecryptor();
 
             using FileStream inFs = new(file.FullName, FileMode.Open);
@@ -98,6 +89,16 @@ namespace WebApplication1.Services
             result.FileInfo = new FileInfo(outFs.Name);
 
             return result;
+        }
+
+        private Aes ConfigureAes(string key, string iv, CipherMode cipherMode, PaddingMode paddingMode)
+        {
+            Aes aes = Aes.Create();
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+            aes.Mode = cipherMode;
+            aes.Padding = paddingMode;
+            return aes;
         }
     }
 }
