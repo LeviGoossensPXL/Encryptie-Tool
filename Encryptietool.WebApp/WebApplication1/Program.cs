4710d5ea133161a171352758fb3cad06d9043d87
levig <levi.goossens107@gmail.com>
2026-04-20 19:36:57 +0200

    change default route

--- Encryptietool.WebApp/WebApplication1/Program.cs
+++ Encryptietool.WebApp/WebApplication1/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=KeyGeneration}/{action=Index}/{id?}");
 
 app.Run();
